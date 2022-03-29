7fcb30611bde7d3fb8b5f33a02891c842d2cd8ae
chenxy <748544926@qq.com>
2022-03-29 10:57:50 +0800

    test

---
+++ src/Test2.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace Cxylibray\Test;
+
+class Test2
+{
+    public static function test(){
+        echo 'Cxylibray\Test\Test2 test';
+    }
+
+}
---
+++ src/test.php
@@ -0,0 +1,2 @@
+<?php
+echo 'is src test.php';
